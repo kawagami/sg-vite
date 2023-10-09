ae000babc22acd20301a398008c10f66d27a61a6
kawagami <joelai1988@gmail.com>
2023-10-09 21:33:18 +0800

    add: scss 全局變數 & layout index

---
+++ src/layout/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+    <div class="layout_container">
+        <!-- 左側 sidebar -->
+        <div class="layout_slider">123</div>
+        
+        <!-- 頂部 navigation -->
+        <div class="layout_tabbar">456</div>
+        
+        <!-- 內容 -->
+        <div class="layout_main">789</div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.layout_container {
+    width: 100%;
+    height: 100vh;
+    background: $color;
+}
+</style>
