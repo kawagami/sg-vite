a55118f5b40576a6798c03b1816e2db8d08599e9
kawagami <joelai1988@gmail.com>
2023-10-09 22:07:54 +0800

    add: 靜態頁面基本設定完成

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -5,12 +5,14 @@
     <div class="layout_container">
         <!-- 左側 sidebar -->
         <div class="layout_slider">123</div>
-        
+
         <!-- 頂部 navigation -->
         <div class="layout_tabbar">456</div>
-        
+
         <!-- 內容 -->
-        <div class="layout_main">789</div>
+        <div class="layout_main">
+            <p style="height: 20000px; background-color: blue;"></p>
+        </div>
     </div>
 </template>
 
@@ -18,6 +20,32 @@
 .layout_container {
     width: 100%;
     height: 100vh;
-    background: $color;
+    background: red;
+
+    .layout_slider {
+        width: $base-menu-width;
+        height: 100vh;
+        background: $base-menu-color;
+    }
+
+    .layout_tabbar {
+        position: fixed;
+        width: calc(100% - $base-menu-width);
+        height: $base-tabbar-height;
+        background: cyan;
+        top: 0;
+        left: $base-menu-width;
+    }
+
+    .layout_main {
+        position: absolute;
+        width: calc(100% - $base-menu-width - ($base-main-padding * 2));
+        height: calc(100vh - $base-tabbar-height - ($base-main-padding * 2));
+        background: yellowgreen;
+        top: $base-tabbar-height;
+        left: $base-menu-width;
+        padding: $base-main-padding;
+        overflow: auto;
+    }
 }
 </style>
