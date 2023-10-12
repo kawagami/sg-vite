5513bb6cd4fb3ad5145d1cd8083cadd15b813e52
kawagami <joelai1988@gmail.com>
2023-10-12 23:09:15 +0800

    add: 全螢幕按鈕，可能有瀏覽器相容性問題

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -1,6 +1,6 @@
 <template>
     <el-button icon="Refresh" size="small" circle @click="updateRefresh" />
-    <el-button icon="FullScreen" size="small" circle />
+    <el-button icon="FullScreen" size="small" circle @click="fullScreen" />
     <el-button icon="Setting" size="small" circle />
 
     <img src="../../../../public/logo.png" style="height: 24px; widows: 24px; margin: 0 10px;">
@@ -27,6 +27,15 @@ let layOutSettingStore = useLayOutSettingStore();
 
 const updateRefresh = () => {
     layOutSettingStore.refresh = !layOutSettingStore.refresh;
+};
+
+const fullScreen = () => {
+    let full = document.fullscreenElement;
+    if (!full) {
+        document.documentElement.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
 };
 </script>
 <script>
