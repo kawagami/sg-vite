d8d4999878755ca1d4672c6a368f4cca7a5fe8af
kawagami <joelai1988@gmail.com>
2023-10-12 10:28:30 +0800

    update: 更新 shell 檔案

--- app.sh
+++ app.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd):/app -w /app -u $(id -u):$(id -g) node:16 $@
+NAME="sg-vite"
+
+# docker run --rm -it -v $(pwd):/app -w /app -u $(id -u):$(id -g) node:16 $@
+docker exec -it -u $(id -u):$(id -g) $NAME $@
--- up.sh
+++ up.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd):/app -w /app -p 5173:5173 -u $(id -u):$(id -g) node:16 npm run dev -- --host
+NAME="sg-vite"
+
+code .
+docker run \
+    --rm \
+    --name $NAME \
+    -it \
+    -v $(pwd):/app \
+    -w /app \
+    -p 5173:5173 \
+    -u $(id -u):$(id -g) \
+    node:16 npm run dev -- --host
